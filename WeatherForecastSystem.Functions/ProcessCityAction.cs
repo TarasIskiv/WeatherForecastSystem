06b55d193720330787dc61922697fbc3826376fe
Taras Iskiv <taras.iskiv@gmail.com>
2023-10-09 23:50:56 +0200

    Configured Http trigger function

---
+++ WeatherForecastSystem.Functions/ProcessCityAction.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+using WeatherForecastSystem.Core.ClientModels;
+using WeatherForecastSystem.Functions.Helpers;
+using WeatherForecastSystem.ServiceBusLogic.Abstraction;
+
+namespace WeatherForecastSystem.Functions;
+
+public class ProcessCityAction : FunctionBase<ProcessCityAction>
+{
+    public ProcessCityAction(IServiceBusMessagingService _messagingService) : base(_messagingService)
+    {
+        
+    }
+    [Function("ProcessCityAction")]
+    public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequestData req)
+    {
+        try
+        {
+            using StreamReader reader = new StreamReader(req.Body, Encoding.UTF8);
+            var data = reader.ReadToEnd();
+            var cityAction = JsonSerializer.Deserialize<CityAction>(data);
+            await _messagingService.SendMessage(cityAction);
+            return req.CreateResponse(HttpStatusCode.Created);
+        }
+        catch (Exception e)
+        {
+            return req.CreateResponse(HttpStatusCode.InternalServerError);
+        }
+    }
+}
